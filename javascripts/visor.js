d62ce411b6675aa8d51e51b9caa1f1d9e9683fba
GUSPRIETO <grutadecas@hotmail.com>
2017-09-12 18:15:15 -0500

    Update visor.js

--- javascripts/visor.js
+++ javascripts/visor.js
@@ -71,7 +71,7 @@ function init() {
         });
         //console.log(response);
     });
-    layerCuadrantes = new esri.layers.ArcGISDynamicMapServiceLayer("https://gis.policia.gov.co:6443/arcgis/rest/services/CAPAS/CUADRANTES/MapServer",{opacity:.70});
+    layerCuadrantes = new esri.layers.ArcGISDynamicMapServiceLayer("https://gisponal.policia.gov.co/arcgis/rest/services/CAPAS/CUADRANTES/MapServer",{opacity:.70});
     mapa.addLayer(layerCuadrantes);
 
 	layermallavial = new esri.layers.ArcGISDynamicMapServiceLayer("https://gis.policia.gov.co:6443/arcgis/rest/services/CAPAS/MALLA_VIAL/MapServer",{opacity:.70});
@@ -222,7 +222,7 @@ function mapReady(map) {
         navigator.geolocation.getCurrentPosition(centerMap, locationError);
     }
     dojo.connect(map, "onClick", executeIdentifyTask);
-    identifyTask = new esri.tasks.IdentifyTask("https://gis.policia.gov.co:6443/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer");
+    identifyTask = new esri.tasks.IdentifyTask("https://gisponal.policia.gov.co/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer");
 
     //Obtain address
     locator = new esri.tasks.Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
@@ -273,9 +273,9 @@ function executeIdentifyTask(evt) {
     mapa.infoWindow.setTitle("Coordenadas");
     mapa.infoWindow.setContent("lat/lon : " + latDelito.toFixed(2) + ", " + lonDelito.toFixed(2));
     mapa.infoWindow.show(evt.mapPoint, mapa.getInfoWindowAnchor(evt.screenPoint));*/
-        var qtCuadrantes = new esri.tasks.QueryTask("https://gis.policia.gov.co:6443/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer/11");	
+        var qtCuadrantes = new esri.tasks.QueryTask("https://gisponal.policia.gov.co/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer/11");	
     var qCuadrantes = new esri.tasks.Query();
-	var qtEstaciones = new esri.tasks.QueryTask("https://gis.policia.gov.co:6443/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer/9");
+	var qtEstaciones = new esri.tasks.QueryTask("https://gisponal.policia.gov.co/arcgis/rest/services/DIJIN/SIDENCO_SinMalla/MapServer/9");
     var qEstaciones = new esri.tasks.Query();
 	qEstaciones.returnGeometry = qCuadrantes.returnGeometry = false;
 	qCuadrantes.outFields = qEstaciones.outFields = ['CODIGO_SIEDCO'];
